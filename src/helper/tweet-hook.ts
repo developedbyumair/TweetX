ad017b2bea6536ff8ab730e6b4f0b282c2961885
Umair Ali <umair@dataspecc.com>
2024-10-22 22:16:59 +0500

    Create tweet-hook.ts

---
+++ src/helper/tweet-hook.ts
@@ -0,0 +1,67 @@
+export const hookFormula = `
+## Hook Templates
+
+### Growth & Success Hooks
+| Template | Example |
+|----------|----------|
+| F*ck it (blank) | F*ck it, here are my secrets on how I went from 1k to 10k in 3 months |
+| Here's (amount) underestimated (hack/tip/trick) | Here's 3 hacks to go lose weight fast |
+| How to (solution) in (time frame) | How to get 100k followers in 2 Months |
+| This hack will save you hours on (insert solution here) | This hack will save you hours of studying for your exam |
+
+### Curiosity & Mystery Hooks
+| Template | Example |
+|----------|----------|
+| Why doesn't anyone talk about this? | Why doesn't anyone talk about this? Paid ads don't work anymore |
+| (blank) things that feel illegal to know | 3 Websites that feel illegal to know |
+| Exposing my secret to (blank) | Exposing my secret to recording a months worth of content in 1 day |
+
+### Problem-Solution Hooks
+| Template | Example |
+|----------|----------|
+| This is why your (action) isn't working | This is why your TikTok strategy isn't working |
+| Here are 3 signs you should (action) | Here are 3 signs you should lose weight |
+| This one mistake could be costing you ($) | This one mistake could be costing you a million dollars |
+| If you're not getting (blank) then you need to be doing (blank) | If you're not getting results on TikTok then you need to improve your hook |
+
+### Aspirational Hooks
+| Template | Example |
+|----------|----------|
+| Imagine if you could (insert result here) | Imagine if you could make money while traveling |
+| These 3 books changed my life | These 3 books changed my life |
+| Life Hack I wish I knew earlier | Life Hack I wish I knew Earlier |
+
+### Controversial & Pattern Interrupt Hooks
+| Template | Example |
+|----------|----------|
+| Do you still believe this (industry) myth? | Do you still believe that deadlifting is bad for your back |
+| This is my most controversial belief about (insert topic here) | This is my most controversial belief about feminism |
+| Everything you knew about (subject) is WRONG! | Everything you knew about the Titanic is wrong! |
+| Don't hate me but (insert hard truth) | Don't hate me but a calorie deficit is the only reason to shed weight |
+
+### Authority & Experience Hooks
+| Template | Example |
+|----------|----------|
+| I tried every (blank) so you don't have to | I tried every workout split so you don't have to |
+| (specific number) things about (niche) I wish I knew earlier | 3 things about dropshipping I wish I knew earlier |
+| If there's one thing you'll never catch me doing, it's this ... | If there's one thing you'll never catch me doing, it's this: I will never take advantage of my clients |
+
+### Platform-Specific Hooks
+| Template | Example |
+|----------|----------|
+| This will change the way you use (blank) | This will change the way you use TikTok |
+| Is it just me, or... | Is it just me, or is Instagram dying |
+| You're wanting to [result] but you're doing (current situation) | You're wanting to blow up on youtube but you're only posting once a month |
+
+## Usage Guidelines
+
+1. **Customization**: Adapt these templates to fit your niche and content style
+2. **Authenticity**: While using these hooks, maintain authenticity in your content
+3. **Testing**: Test different hooks with your audience to see which perform best
+4. **Platform Adaptation**: Modify hooks based on the platform you're using
+5. **Value First**: Ensure your content delivers on the promise made in the hook
+
+---
+*Note: These templates are meant to inspire creative content creation while maintaining ethical marketing practices. Always prioritize providing genuine value to your audience.*
+
+`
